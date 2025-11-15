76b541ebf4ea7cfe33074364482a3498ec81ce74
mhamed kbt <mhamedkhebit@gmail.com>
2025-11-15 20:02:55 +0100

    m

---
+++ src/main/java/com/example/AuthServicePrj/AuthServicePrjApplication.java
@@ -0,0 +1,13 @@
+package com.example.AuthServicePrj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServicePrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServicePrjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AuthServicePrj/AuthServicePrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AuthServicePrj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServicePrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
